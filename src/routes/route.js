ea67c049382c98dde4088889c1f125abc0cef84f
CCAnkit <ankitgupta9828@gmail.com>
2022-03-25 14:38:08 +0530

    update

---
+++ src/routes/route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../controllers/mainController.js");
+
+
+router.post("/", );   //
+
+router.post("/", );   //
+
+router.get("/", );   //
+
+
+module.exports = router;
